bb39f34cd61608e42306986c94e0b86286ae8cc9
ralrobaili <ralrobaili@gmail.com>
2026-03-05 01:08:46 +0300

    Initial Commit

---
+++ wezanapp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  wezanapp
+//
+//  Created by raghad alenezi on 16/09/1447 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ wezanapp/wezanappApp.swift
@@ -0,0 +1,17 @@
+//
+//  wezanappApp.swift
+//  wezanapp
+//
+//  Created by raghad alenezi on 16/09/1447 AH.
+//
+
+import SwiftUI
+
+@main
+struct wezanappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
